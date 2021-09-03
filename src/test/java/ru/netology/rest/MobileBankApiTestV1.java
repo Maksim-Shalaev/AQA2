04b39a6e80cf9edadc35a277337844ac0534b144
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-09-04 00:47:29 +0300

    updated files build, test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,7 +1,7 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
@@ -13,9 +13,12 @@ class MobileBankApiTestV1 {
           .baseUri("http://localhost:9999/api/v1")
       // Выполняемые действия
       .when()
+
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
 }
